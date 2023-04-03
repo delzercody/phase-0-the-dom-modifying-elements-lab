3bb359a5dd8893b235ad08b55e620a76da45888c
Cody Delzer <delzercody@gmail.com>
2023-04-03 17:49:40 -0400

    completed dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+const newHeader = document.createElement("h1")
+const h1 = document.getElementById("victory")
+newHeader.id = "victory"
+newHeader.innerHTML = "Cody is the champion"
